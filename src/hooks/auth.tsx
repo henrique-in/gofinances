27badeff8c540e8035c79e42659e64f7231df7a9
Henrique <henrique.fsa97@hotmail.com>
2022-09-25 11:26:24 -0300

    social login apple and google

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,41 +1,108 @@
-import React, { createContext, ReactNode, useContext} from "react";
+import React, { createContext, ReactNode, useContext, useState } from "react";
 
-interface AuthProviderProps{
-    children: ReactNode;
+import * as AuthSession from "expo-auth-session";
+import * as AppleAuthentication from "expo-apple-authentication";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+interface AuthProviderProps {
+  children: ReactNode;
 }
 
-interface User{
-    id: string;
-    name:string;
-    email:string;
-    photo?:string;
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
 }
 
-interface IAuthContextData{
-    user: User
+interface IAuthContextData {
+  user: User;
+  signInWithGoogle(): Promise<void>;
+  signInWithApple(): Promise<void>;
 }
 
-const AuthContext = createContext({ } as IAuthContextData)
+interface AuthorizationReponse {
+  params: {
+    access_token: string;
+  };
+  type: string;
+}
 
-function AuthProvider({children}: AuthProviderProps){
-    const user = {
-        id:'434',
-        name:"Henrique",
-        email: 'henrique@s.com'
+const AuthContext = createContext({} as IAuthContextData);
 
+function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User>({} as User);
+
+  async function signInWithGoogle() {
+    try {
+      const RESPONSE_TYPE = "token";
+      const SCOPE = encodeURI("profile email");
+
+      const authUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+
+      const { type, params } = (await AuthSession.startAsync({
+        authUrl,
+      })) as AuthorizationReponse;
+
+      if (type === "success") {
+        const response = await fetch(
+          `https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token=${params.access_token}`
+        );
+        const userInfo = await response.json();
+        setUser({
+          id: userInfo.id,
+          email: userInfo.email,
+          name: userInfo.given_name,
+          photo: userInfo.picture,
+        });
+        await AsyncStorage.setItem("gofinances:user", JSON.stringify(userInfo));
+      }
+    } catch (error: any) {
+      65375;
+      throw new Error(error);
     }
-    return(
-        <AuthContext.Provider value={{user}}>
-            {children}
-        </AuthContext.Provider>
-    )
+  }
+
+  async function signInWithApple() {
+    try {
+      const credential = await AppleAuthentication.signInAsync({
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+          AppleAuthentication.AppleAuthenticationScope.EMAIL,
+        ],
+      });
+      if (credential) {
+        const userLogged = {
+          id: String(credential.user),
+          email: credential.email!,
+          name: credential.fullName!.givenName!,
+          photo: undefined,
+        };
+        setUser(userLogged);
+        await AsyncStorage.setItem(
+          "gofinances:user",
+          JSON.stringify(userLogged)
+        );
+      }
+    } catch (error: any) {
+      throw new Error(error);
+    }
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, signInWithGoogle, signInWithApple }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
+function useAuth() {
+  const context = useContext(AuthContext);
 
-function useAuth(){
-    const context = useContext(AuthContext);
-
-    return context
+  return context;
 }
 
-export { AuthProvider, useAuth }
+export { AuthProvider, useAuth };
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,46 +1,67 @@
-import React,{useContext} from 'react';
-import { View } from 'react-native';
-import { RFValue } from 'react-native-responsive-fontsize';
+import React from "react";
+import { Alert } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
-import AppSvg from '../../assets/apple.svg'
-import GoogleSvg from '../../assets/google.svg'
-import LogoSvg from '../../assets/logo.svg'
-import SignSocialButton from '../../components/SignSocialButton';
-import { useAuth } from '../../hooks/auth';
+import AppSvg from "../../assets/apple.svg";
+import GoogleSvg from "../../assets/google.svg";
+import LogoSvg from "../../assets/logo.svg";
+import SignSocialButton from "../../components/SignSocialButton";
+import { useAuth } from "../../hooks/auth";
 
- import  * as S from './styles';
+import * as S from "./styles";
 
-export function SignIn(){
-    const {user} = useAuth()
-    
+export function SignIn() {
+  const { signInWithGoogle, signInWithApple } = useAuth();
+
+  async function handleSignInWithGoogle() {
+    try {
+      await signInWithGoogle();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possível conectar a conta Google");
+    }
+  }
+
+  async function handleSignInWithApple() {
+    try {
+      await signInWithApple();
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Não foi possível conectar a Apple");
+    }
+  }
+
   return (
     <S.Container>
-        <S.Header>
-            <S.TitleWrapper>
-                <LogoSvg
-                  width={RFValue(120)}
-                  height={RFValue(68)}
-                />
-                <S.Title>
-                    Controle suas {'\n'} 
-                    finanças de forma{'\n'}
-                     muito simples
-                </S.Title>
-                <S.SignInTitle>
-                    Faça seu login com{'\n'}
-                    uma das contas abaixo
-                </S.SignInTitle>
-            </S.TitleWrapper>
-        </S.Header>
+      <S.Header>
+        <S.TitleWrapper>
+          <LogoSvg width={RFValue(120)} height={RFValue(68)} />
+          <S.Title>
+            Controle suas {"\n"}
+            finanças de forma{"\n"}
+            muito simples
+          </S.Title>
+          <S.SignInTitle>
+            Faça seu login com{"\n"}
+            uma das contas abaixo
+          </S.SignInTitle>
+        </S.TitleWrapper>
+      </S.Header>
 
-        <S.Footer>
-            <S.FooterWrapper>
-                <SignSocialButton title='Entrar com Google' svg={GoogleSvg}/>
-                <SignSocialButton title='Entrar com Google' svg={AppSvg}/>
-            </S.FooterWrapper>
-        </S.Footer>
-
+      <S.Footer>
+        <S.FooterWrapper>
+          <SignSocialButton
+            title="Entrar com Google"
+            svg={GoogleSvg}
+            onPress={handleSignInWithGoogle}
+          />
+          <SignSocialButton
+            title="Entrar com Apple"
+            svg={AppSvg}
+            onPress={handleSignInWithApple}
+          />
+        </S.FooterWrapper>
+      </S.Footer>
     </S.Container>
-  )
+  );
 }
-
